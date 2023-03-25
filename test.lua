fba9351cdd1eb84f66d98bb6f4e737896f860e63
Simoniko <52357715+Simoniko@users.noreply.github.com>
2023-03-25 20:56:51 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Simoniko/pd2-mod/main/mod.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Simoniko/pd2-mod/main/mod.lua"))()
